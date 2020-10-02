718d633b6f4be1911b6828b602e75e4607ed0e22
Nazia-Islam <naziasebelly@gmail.com>
2020-10-03 04:39:15 +0600

    burj-al-arab-client

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,12 +1,82 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { UserContext } from '../../App';
+import { Button } from '@material-ui/core';
+import Bookings from '../Bookings/Bookings';
 
 const Book = () => {
     const {bedType} = useParams();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    
+    const [selectedDate, setSelectedDate] = useState({
+        checkIn: new Date(), checkOut: new Date()
+    });
+    const handleCheckInDate = (date) => {
+        const newDates = {...selectedDate};
+        newDates.checkIn = date;
+        setSelectedDate(newDates);
+    };
+    const handleCheckOutDate = (date) => {
+        const newDates = {...selectedDate};
+        newDates.checkOut = date;
+        setSelectedDate(newDates);
+    };
+
+    const handleBooking = () => {
+        const newbooking = {...loggedInUser, ...selectedDate};
+        console.log(newbooking);
+        fetch('http://localhost:5000/addBooking', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newbooking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
+            <h1>Hello, {loggedInUser.name}! Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Date picker inline"
+                    value={selectedDate.checkIn}
+                    onChange={handleCheckInDate}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                    />
+                    <KeyboardDatePicker
+                    margin="normal"
+                    id="date-picker-dialog"
+                    label="Date picker dialog"
+                    format="dd/MM/yyyy"
+                    value={selectedDate.checkOut}
+                    onChange={handleCheckOutDate}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                    />
+                </Grid>
+                <Button onClick={handleBooking} variant="contained" color="primary">Book Now</Button>
+            </MuiPickersUtilsProvider>
+            <Bookings></Bookings>
         </div>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,57 @@
 import React, { useContext } from 'react';
-
+import {UserContext} from '../../App';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    
+    const handleGoogleSignIn = () => {
+
+        firebase.auth().signInWithPopup(googleProvider)
+        .then(function(result) {
+            const {displayName, email} = result.user;
+            const signedInUser = {name: displayName, email};
+            setLoggedInUser(signedInUser);
+            storeAuthToken();
+          })
+          .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+        .then(function(idToken) {
+            sessionStorage.setItem('token', idToken);
+            history.replace(from);
+          }).catch(function(error) {
+            // Handle error
+          });
+    }
+
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Signin with Google</button>
         </div>
     );
 };
